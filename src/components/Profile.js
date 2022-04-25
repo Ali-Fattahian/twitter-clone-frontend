5726edff0e96e7eb5c606e6ee4bb4011fe05defc
Ali-Fattahian <ali.fattahian1378@gmail.com>
2022-04-26 00:01:53 +0430

    style Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,13 +5,21 @@ const Profile = () => {
   return (
     <section className={classes.profile}>
       <div className={classes["profile__top"]}>
-        <div className={classes["profile__top-left"]}></div>
+        <div className={classes["profile__top-left"]}>
+          <div id={classes.icon}>
+            <i className="fa fa-chevron-left"></i>
+          </div>
+          <div>
+            <h3>Full Name</h3>
+            <p>1200 Tweets</p>
+          </div>
+        </div>
         <div></div>
       </div>
       <img src={ProfilePicture} alt="header" />
       <div className={classes["profile__bottom"]}>
         <div className={classes["profile__bottom-middle"]}>
-          <img src={ProfilePicture} alt='profile' />
+          <img src={ProfilePicture} alt="profile" />
           <button className="btn">Follow</button>
         </div>
         <div className={classes["profile__bottom-bottom"]}>
@@ -27,18 +35,26 @@ const Profile = () => {
             </p>
           </div>
           <p className={classes["user-date-joined"]}>Joined November 2017</p>
-          <div className={classes["user-follow"]}>
-            <span>193</span>
-            <span id={classes["user-follow__text"]}>Following</span>
-          </div>
-          <div className={classes["user-follow"]}>
-            <span>842</span>
-            <span id={classes["user-follow__text"]}>Following</span>
+          <div className={classes.follow}>
+            <div className={classes["user-follow"]}>
+              <span>193</span>
+              <p id={classes["user-follow__text"]}>Following</p>
+            </div>
+            <div className={classes["user-follow"]}>
+              <span>842</span>
+              <p id={classes["user-follow__text"]}>Followers</p>
+            </div>
           </div>
         </div>
       </div>
     </section>
   );
 };
+
+// Images should be a link to the image
+// Fullname at the top and the tweet count are links to the profile page
+// Follow button is Following turns into Unfollow (red) (244, 33, 46) on hover. at least in dark mode.
+// For not followings, it is a Follow text (black color) and (#eff3f4) background and gets a little darker on hover.
+// Following and Follower number at the bottom show real peaple
 
 export default Profile;
