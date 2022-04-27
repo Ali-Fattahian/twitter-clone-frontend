ea252906d9dee75a3ed25b4f44da36e7c44cc1b4
Ali-Fattahian <ali.fattahian1378@gmail.com>
2022-04-27 20:06:59 +0430

    add navlinks for navigation comp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,46 @@
-import classes from './Navigation.module.css'
+import { NavLink } from "react-router-dom";
+import classes from "./Navigation.module.css";
 
 const Navigation = () => {
   return (
     <section className={classes["nav-container"]}>
       <nav className={classes.nav}>
-      <div className={classes["nav__item"]} id={classes['twitter-icon']}>
+        <NavLink to="/" className={classes["nav__item"]}>
           <i class="fa fa-twitter"></i>
-        </div>  
-        <div className={classes["nav__item"]}>
+        </NavLink>
+        <NavLink
+          to="/"
+          className={classes['nav__item']}
+          >
           <i class="fa fa-home"></i>
           <p>Home</p>
-        </div>
-        <div className={classes["nav__item"]}>
+        </NavLink>
+        <NavLink
+          to="/explore"
+          className={classes['nav__item']}
+        >
           <i class="fa fa-hashtag"></i>
           <p>Explore</p>
-        </div>
-        <div className={classes["nav__item"]}>
+        </NavLink>
+        {/* <NavLink to='/' className={classes["nav__item"]}>
           <i class="fa fa-envelope-o"></i>
           <p>Messages</p>
-        </div>
-        <div className={classes["nav__item"]}>
+        </NavLink> */}
+        <NavLink
+          to="/bookmarks"
+          className={classes['nav__item']}>
           <i class="fa fa-bookmark-o"></i>
           <p>Bookmarks</p>
-        </div>
-        <div className={classes["nav__item"]}>
+        </NavLink>
+        <NavLink
+          to="/:username"
+          className={classes['nav__item']}
+        >
           <i class="far fa-user-circle"></i>
           <p>Profile</p>
-        </div>
-        <div className={classes['nav__item']} id={classes['nav__tweet']}>
-            <button className='btn'>Tweet</button>
+        </NavLink>
+        <div className={classes["nav__item"]} id={classes["nav__tweet"]}>
+          <button className="btn">Tweet</button>
         </div>
       </nav>
     </section>
