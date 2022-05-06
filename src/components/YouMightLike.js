600cef4c326f3533514d791ec032a5f08525dd4b
Ali-Fattahian <ali.fattahian1378@gmail.com>
2022-05-06 20:54:21 +0430

    add real data to youmightlike component

--- src/components/YouMightLike.js
+++ src/components/YouMightLike.js
@@ -1,48 +1,50 @@
+import { useEffect, useState, useCallback } from "react";
 import classes from "./YouMightLike.module.css";
 import ProfilePicture from "./Tweet/default_profile.png";
 import FollowButton from "./FollowButton";
+import axiosInstance from "../axios";
+import axios from "axios";
 
 const YouMightLike = () => {
+  const [suggestedUsers, setSuggestedUsers] = useState([]);
+  const isLoggedIn = !!localStorage.getItem("access_token");
+
+  const getSuggestedUsers = useCallback(async () => {
+    let response;
+    if (!isLoggedIn) {
+      response = await axios.get("http://127.0.0.1:8000/api/suggested-users");
+    } else {
+      response = await axiosInstance.get(
+        "http://127.0.0.1:8000/api/suggested-users"
+      );
+    }
+    if (response.status === 200) setSuggestedUsers(response.data);
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    getSuggestedUsers()
+  }, [getSuggestedUsers])
+
   return (
     <section className={classes["you-might-like"]}>
       <p>Who to follow</p>
       <div className={classes["suggested-users"]}>
-        <div className={classes["suggested-user"]}>
-          <div className={classes["suggested-user__left"]}>
-            <img src={ProfilePicture} alt="profile" />
-            <div className={classes["user-info"]}>
-              <p id={classes["fullname"]}>Some Dude</p>
-              <p id={classes["username"]}>@some_dude</p>
+        {suggestedUsers.map((user) => (
+          <div className={classes["suggested-user"]}>
+            <div className={classes["suggested-user__left"]}>
+              <img src={ProfilePicture} alt="profile" />
+              <div className={classes["user-info"]}>
+                <p
+                  id={classes["fullname"]}
+                >{`${user.firstname} ${user.lastname}`}</p>
+                <p id={classes["username"]}>@{user.username}</p>
+              </div>
+            </div>
+            <div className={classes["suggested-user__right"]}>
+              <FollowButton color="#fff" backgroundColor="#000" />
             </div>
           </div>
-          <div className={classes["suggested-user__right"]}>
-            <FollowButton color='#fff' backgroundColor='#000' />
-          </div>
-        </div>
-        <div className={classes["suggested-user"]}>
-          <div className={classes["suggested-user__left"]}>
-            <img src={ProfilePicture} alt="profile" />
-            <div className={classes["user-info"]}>
-              <p id={classes["fullname"]}>Some Dude</p>
-              <p id={classes["username"]}>@some_dude</p>
-            </div>
-          </div>
-          <div className={classes["suggested-user__right"]}>
-            <FollowButton color='#fff' backgroundColor='#000' />
-          </div>
-        </div>
-        <div className={classes["suggested-user"]}>
-          <div className={classes["suggested-user__left"]}>
-            <img src={ProfilePicture} alt="profile" />
-            <div className={classes["user-info"]}>
-              <p id={classes["fullname"]}>Some Dude</p>
-              <p id={classes["username"]}>@some_dude</p>
-            </div>
-          </div>
-          <div className={classes["suggested-user__right"]}>
-            <FollowButton color='#fff' backgroundColor='#000' />
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
