9805f1c05d372cf2b110c72651304df293895ec0
Ali-Fattahian <ali.fattahian1378@gmail.com>
2022-05-24 02:20:11 +0430

    add yup and formik for handling sign up and finish the fields validation

--- src/pages/Auth/Signup.js
+++ src/pages/Auth/Signup.js
@@ -1,253 +1,185 @@
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import classes from "./Signup.module.css";
 import loginClasses from "./Login.module.css";
 import axios from "axios";
+import Ovelay from "../../components/Modal/Overlay";
+import * as Yup from "yup";
 
 const Signup = () => {
-  // const [enteredFirstname, setEnteredFirstname] = useState("");
-  // const [enteredLastname, setEnteredLastname] = useState("");
-  // const [enteredEmail, setEnteredEmail] = useState("");
-  // const [enteredUsername, setEnteredUsername] = useState("");
-  // const [enteredPassword, setEnteredPassword] = useState("");
+  const navigate = useNavigate();
+  const [message, setMessage] = useState(null)
+  let isEmailValid;
+  let isUsernameValid;
 
-  // const [firstnameTouched, setFirstnameTouched] = useState(false);
-  // const [lastnameTouched, setLastnameTouched] = useState(false);
-  // const [emailTouched, setEmailTouched] = useState(false);
-  // const [usernameTouched, setUsernameTouched] = useState(false);
-  // const [passwordTouched, setPasswordTouched] = useState(false);
+  const SignUpSchema = Yup.object().shape({
+    firstname: Yup.string()
+      .max(50, "Too long!")
+      .required("First name is required"),
+    lastname: Yup.string()
+      .max(50, "Too long!")
+      .required("Last name is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password should be at least 8 characters"),
+    email: Yup.string()
+      .email("Invalid email")
+      .required("Email is required")
+      .test(
+        "checkDupEmail",
+        "A user with this email already exists",
+        async (value) => {
+          await fetch("http://127.0.0.1:8000/api/check-email/", {
+            method: "POST",
+            body: JSON.stringify({
+              email: value,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              isUsernameValid = data !== true;
+            });
+          return isUsernameValid;
+        }
+      ),
+    username: Yup.string()
+      .required("Username is required")
+      .test(
+        "checkDupUsername",
+        "A user with this username already exists",
+        async (value) => {
+          await fetch("http://127.0.0.1:8000/api/check-username/", {
+            method: "POST",
+            body: JSON.stringify({
+              username: value,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              isEmailValid = data !== true;
+            });
+          return isEmailValid;
+        }
+      ),
+  });
 
-  // const [formIsValid, setFormIsValid] = useState(false);
-
-  // let message = null;
-
-  // const firstnameChangeHandler = (e) => {
-  //   setEnteredFirstname(e.target.value);
-  //   if (!enteredFirstname.trim().length > 0 && firstnameTouched) {
-  //     message = "Please provide a first name";
-
-  //     return;
-  //   }
-  // };
-
-  // const lastnameChangeHandler = (e) => {
-  //   setEnteredLastname(e.target.value);
-  //   if (!enteredLastname.trim().length > 0 && lastnameTouched) {
-  //     message = "Please provide a last name";
-  //     return;
-  //   }
-  // };
-
-  // const emailChangeHandler = (e) => {
-  //   setEnteredEmail(e.target.value);
-  //   if (!enteredEmail.trim().length > 0 && emailTouched) {
-  //     message = "Email must not be empty";
-  //     return;
-  //   }
-
-  //   if (
-  //     enteredEmail.trim().length > 0 &&
-  //     emailTouched &&
-  //     !enteredEmail.includes("@")
-  //   ) {
-  //     message = "Make sure your email address is valid";
-  //   }
-  // };
-
-  // const usernameChangeHandler = (e) => {
-  //   setEnteredUsername(e.target.value);
-  //   if (!enteredUsername.trim().length > 0 && usernameTouched) {
-  //     message = "Please provide a username";
-  //     return;
-  //   }
-  // };
-
-  // const passwordChangeHandler = (e) => {
-  //   setEnteredPassword(e.target.value);
-  //   if (!enteredPassword.trim().length > 0 && passwordTouched) {
-  //     message = "Please provide a password";
-  //     return;
-  //   }
-
-  //   if (enteredPassword.trim().length < 8 && passwordTouched) {
-  //     message = "Make sure your password is at least 8 characters";
-  //     return;
-  //   }
-  // };
-
-  // async function sendUserData(
-  //   enteredEmail,
-  //   enteredUsername,
-  //   enteredFirstname,
-  //   enteredLastname,
-  //   enteredPassword
-  // ) {
-  //   await fetch("http://127.0.0.1:8000/api/", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       email: enteredEmail,
-  //       username: enteredUsername,
-  //       firstname: enteredFirstname,
-  //       lastname: enteredLastname,
-  //       password: enteredPassword,
-  //     }),
-  //   }).then(res => res.json()).then(data => console.log(data))
-  // }
-
-  // const formSubmitHandler = (e) => {
-  //   e.preventDefault();
-
-  //   setFirstnameTouched(true);
-  //   setLastnameTouched(true);
-  //   setEmailTouched(true);
-  //   setUsernameTouched(true);
-  //   setPasswordTouched(true);
-
-  //   if (
-  //     enteredEmail.trim().length > 0 &&
-  //     enteredEmail.includes("@") &&
-  //     enteredPassword.trim().length > 7 &&
-  //     enteredFirstname.trim().length > 0 &&
-  //     enteredLastname.trim().length > 0 &&
-  //     enteredUsername.trim().length > 0
-  //   )
-  //     setFormIsValid(true);
-  //   if (formIsValid)
-  //     sendUserData(
-  //       enteredEmail,
-  //       enteredUsername,
-  //       enteredFirstname,
-  //       enteredLastname,
-  //       enteredPassword
-  //     );
-  // };
-
-  const username = useRef("");
-  const email = useRef("");
-  const firstname = useRef("");
-  const lastname = useRef("");
-  const password = useRef("");
-  const [message, setMessage] = useState("");
-  const navigate = useNavigate()
-
-  const formSubmitHandler = (e) => {
-    e.preventDefault();
-    const usernameValue = username.current.value;
-    const emailValue = email.current.value;
-    const firstnameValue = firstname.current.value;
-    const lastnameValue = lastname.current.value;
-    const passwordValue = password.current.value;
-
-    if (
-      !usernameValue.trim() ||
-      !emailValue.trim() ||
-      !firstnameValue.trim() ||
-      !lastnameValue.trim() ||
-      !passwordValue.trim()
-    ) {
-      setMessage("All the fields are required.");
-    }
-
-    if (!emailValue.includes("@")) {
-      setMessage("Please enter a valid email.");
-    }
-
-    if (passwordValue.trim().length < 8) {
-      setMessage("The password should be at least 8 characters.");
-    }
-        // if (!res)
-        //   setMessage("A user with the same username or email already exists.");
-        // if (res.status === 201)
-        //   setMessage(
-        //     "There was a netword error, make sure you are connected to the internet."
-    //     //   );
-    //   })
-    //   .catch((err) => {
-    //     setMessage(err.message);
-    //   });
-    // setMessage("");
+  const formSubmitHandler = (props) => {
     const sendData = async () => {
-        await axios.post("http://127.0.0.1:8000/api/", {
-        email: emailValue,
-        username: usernameValue,
-        password: passwordValue,
-        firstname: firstnameValue,
-        lastname: lastnameValue
-      }).then(res => setMessage(res.message)).catch(err => console.log(err))
-    }
+      await axios
+        .post("http://127.0.0.1:8000/api/", {
+          email: props.email,
+          username: props.username,
+          password: props.password,
+          firstname: props.firstname,
+          lastname: props.lastname,
+        })
+        .then((res) => {
+          console.log(res);
+          if (res.status === 204) {
+            setMessage(
+              "Your account was created, we've sent a confirmation email to you, make sure to check spam folder if you can't find it.\nThe link will expire in 10 minutes."
+            );
+          } else if (res.status === 400) {
+            setMessage(res.response.data);
+          }
+        });
+    };
 
-    sendData()
+    sendData();
   };
 
   return (
-    <section className={loginClasses["page-container"]}>
-      <div className={loginClasses["form-container"]}>
-        <form className={classes.form} onSubmit={formSubmitHandler}>
-          {message && (
-            <div className={classes["message-container"]}>
-              <p className={classes.message}>{message}</p>
-            </div>
-          )}
-          <div className={classes["form-section"]}>
-            <i style={{cursor:'pointer'}} className="fa fa-chevron-left" onClick={() => navigate(-1)}></i>
-          </div>
-          <div className={classes["form-section"]} id={classes["user-name"]}>
-            <input
-              type="text"
-              name="firstname"
-              placeholder="First name..."
-              // onChange={firstnameChangeHandler}
-              ref={firstname}
-            />
-            <input
-              type="text"
-              name="lastname"
-              placeholder="Last name..."
-              // onChange={lastnameChangeHandler}
-              ref={lastname}
-            />
-          </div>
-          <div className={classes["form-section"]}>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email..."
-              // onChange={emailChangeHandler}
-              ref={email}
-            />
-          </div>
-          <div className={classes["form-section"]}>
-            <input
-              type="text"
-              name="username"
-              placeholder="Username..."
-              // onChange={usernameChangeHandler}
-              ref={username}
-            />
-          </div>
-          <div className={classes["form-section"]}>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password..."
-              // onChange={passwordChangeHandler}
-              ref={password}
-            />
-          </div>
-          <div className={classes["form-section"]}>
-            <button type="submit" className="btn">
-              Next
-            </button>
-          </div>
-          <div className={classes["form-section"]}>
-            <p id={classes['already-have-account']}>Already have an account? <Link to='/login'>Sign in</Link></p>
-          </div>
-        </form>
-      </div>
-    </section>
+    <React.Fragment>
+      {/* {!!message && hasStarted && !isLoading && <Ovelay />}
+      {!!message && hasStarted && !isLoading && (
+        <ErrorMessage errorMessage={message} onClose={messageCloseHandler} />
+      )} */}
+      <section className={loginClasses["page-container"]}>
+        <div className={loginClasses["form-container"]}>
+          <Formik
+            initialValues={{
+              firstname: "",
+              lastname: "",
+              email: "",
+              password: "",
+              username: "",
+            }}
+            onSubmit={formSubmitHandler}
+            validationSchema={SignUpSchema}
+          >
+            {({ isSubmitting }) => (
+              <Form className={classes.form}>
+                <div className={classes["form-section"]}>
+                  <i
+                    style={{ cursor: "pointer", color: "#000" }}
+                    className="fa fa-chevron-left"
+                    onClick={() => navigate(-1)}
+                  ></i>
+                </div>
+                <div
+                  className={classes["form-section"]}
+                  id={classes["user-name"]}
+                >
+                  <Field
+                    type="text"
+                    name="firstname"
+                    placeholder="First name..."
+                  />
+                  <ErrorMessage name="firstname" />
+                  <Field
+                    type="text"
+                    name="lastname"
+                    placeholder="Last name..."
+                  />
+                  <ErrorMessage name="lastname" />
+                </div>
+                <div className={classes["form-section"]}>
+                  <Field type="email" name="email" placeholder="Email..." />
+                  <ErrorMessage name="email" />
+                </div>
+                <div className={classes["form-section"]}>
+                  <Field
+                    type="text"
+                    name="username"
+                    placeholder="Username..."
+                  />
+                  <ErrorMessage name="username" />
+                </div>
+                <div className={classes["form-section"]}>
+                  <Field
+                    type="password"
+                    name="password"
+                    placeholder="Password..."
+                  />
+                  <ErrorMessage name="password" />
+                </div>
+                <div className={classes["form-section"]}>
+                  <button
+                    type="submit"
+                    className="btn"
+                    disabled={!SignUpSchema.isValid}
+                  >
+                    Next
+                  </button>
+                </div>
+                <div className={classes["form-section"]}>
+                  <p id={classes["already-have-account"]}>
+                    Already have an account? <Link to="/login">Sign in</Link>
+                  </p>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </section>
+    </React.Fragment>
   );
 };
 
