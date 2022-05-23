1a2fc8a8af3e6cd21329260b8afdebdaa6ecaf43
Ali-Fattahian <ali.fattahian1378@gmail.com>
2022-05-24 02:45:02 +0430

    add success message for sign up and clean up the file

--- src/pages/Auth/Signup.js
+++ src/pages/Auth/Signup.js
@@ -1,15 +1,14 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import classes from "./Signup.module.css";
 import loginClasses from "./Login.module.css";
 import axios from "axios";
-import Ovelay from "../../components/Modal/Overlay";
 import * as Yup from "yup";
 
 const Signup = () => {
   const navigate = useNavigate();
-  const [message, setMessage] = useState(null)
+  const [message, setMessage] = useState(null);
   let isEmailValid;
   let isUsernameValid;
 
@@ -70,116 +69,127 @@ const Signup = () => {
       ),
   });
 
-  const formSubmitHandler = (props) => {
+  const formSubmitHandler = (values, { resetForm }) => {
+    console.log(values);
     const sendData = async () => {
-      await axios
-        .post("http://127.0.0.1:8000/api/", {
-          email: props.email,
-          username: props.username,
-          password: props.password,
-          firstname: props.firstname,
-          lastname: props.lastname,
-        })
-        .then((res) => {
-          console.log(res);
-          if (res.status === 204) {
-            setMessage(
-              "Your account was created, we've sent a confirmation email to you, make sure to check spam folder if you can't find it.\nThe link will expire in 10 minutes."
-            );
-          } else if (res.status === 400) {
-            setMessage(res.response.data);
-          }
-        });
+      await axios.post("http://127.0.0.1:8000/api/", {
+        email: values.email,
+        username: values.username,
+        password: values.password,
+        firstname: values.firstname,
+        lastname: values.lastname,
+      });
     };
 
     sendData();
+    resetForm();
+    setMessage(
+      "Your account was created, we've sent a confirmation email to you, make sure to check spam folder if you can't find it.\nThe link will expire in 10 minutes."
+    );
   };
 
   return (
-    <React.Fragment>
-      {/* {!!message && hasStarted && !isLoading && <Ovelay />}
-      {!!message && hasStarted && !isLoading && (
-        <ErrorMessage errorMessage={message} onClose={messageCloseHandler} />
-      )} */}
-      <section className={loginClasses["page-container"]}>
-        <div className={loginClasses["form-container"]}>
-          <Formik
-            initialValues={{
-              firstname: "",
-              lastname: "",
-              email: "",
-              password: "",
-              username: "",
-            }}
-            onSubmit={formSubmitHandler}
-            validationSchema={SignUpSchema}
-          >
-            {({ isSubmitting }) => (
-              <Form className={classes.form}>
-                <div className={classes["form-section"]}>
-                  <i
-                    style={{ cursor: "pointer", color: "#000" }}
-                    className="fa fa-chevron-left"
-                    onClick={() => navigate(-1)}
-                  ></i>
-                </div>
-                <div
-                  className={classes["form-section"]}
-                  id={classes["user-name"]}
+    <section className={loginClasses["page-container"]}>
+      <div className={loginClasses["form-container"]}>
+        <Formik
+          initialValues={{
+            firstname: "",
+            lastname: "",
+            email: "",
+            password: "",
+            username: "",
+          }}
+          onSubmit={formSubmitHandler}
+          validationSchema={SignUpSchema}
+        >
+          {({ isSubmitting }) => (
+            <Form className={classes.form}>
+              <div className={classes["form-section"]}>
+                <i
+                  style={{ cursor: "pointer", color: "#000" }}
+                  className="fa fa-chevron-left"
+                  onClick={() => navigate(-1)}
+                ></i>
+              </div>
+              <div
+                className={classes["form-section"]}
+                id={classes["user-name"]}
+              >
+                <Field
+                  type="text"
+                  name="firstname"
+                  placeholder="First name..."
+                />
+                <ErrorMessage
+                  name="firstname"
+                  className={classes["err-msg"]}
+                  component="div"
+                />
+                <Field type="text" name="lastname" placeholder="Last name..." />
+                <ErrorMessage
+                  name="lastname"
+                  className={classes["err-msg"]}
+                  component="div"
+                />
+              </div>
+              <div className={classes["form-section"]}>
+                <Field type="email" name="email" placeholder="Email..." />
+                <ErrorMessage
+                  name="email"
+                  className={classes["err-msg"]}
+                  component="div"
+                />
+              </div>
+              <div className={classes["form-section"]}>
+                <Field type="text" name="username" placeholder="Username..." />
+                <ErrorMessage
+                  name="username"
+                  className={classes["err-msg"]}
+                  component="div"
+                />
+              </div>
+              <div className={classes["form-section"]}>
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Password..."
+                />
+                <ErrorMessage
+                  name="password"
+                  className={classes["err-msg"]}
+                  component="div"
+                />
+              </div>
+              <div className={classes["form-section"]}>
+                <button
+                  type="submit"
+                  className="btn"
+                  disabled={!SignUpSchema.isValid}
                 >
-                  <Field
-                    type="text"
-                    name="firstname"
-                    placeholder="First name..."
-                  />
-                  <ErrorMessage name="firstname" />
-                  <Field
-                    type="text"
-                    name="lastname"
-                    placeholder="Last name..."
-                  />
-                  <ErrorMessage name="lastname" />
-                </div>
-                <div className={classes["form-section"]}>
-                  <Field type="email" name="email" placeholder="Email..." />
-                  <ErrorMessage name="email" />
-                </div>
-                <div className={classes["form-section"]}>
-                  <Field
-                    type="text"
-                    name="username"
-                    placeholder="Username..."
-                  />
-                  <ErrorMessage name="username" />
-                </div>
-                <div className={classes["form-section"]}>
-                  <Field
-                    type="password"
-                    name="password"
-                    placeholder="Password..."
-                  />
-                  <ErrorMessage name="password" />
-                </div>
-                <div className={classes["form-section"]}>
-                  <button
-                    type="submit"
-                    className="btn"
-                    disabled={!SignUpSchema.isValid}
+                  Next
+                </button>
+                {message && (
+                  <p
+                    style={{
+                      marginTop: "5px",
+                      color: "green",
+                      textAlign: "center",
+                    }}
                   >
-                    Next
-                  </button>
-                </div>
-                <div className={classes["form-section"]}>
-                  <p id={classes["already-have-account"]}>
-                    Already have an account? <Link to="/login">Sign in</Link>
+                    {message}
                   </p>
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
-      </section>
-    </React.Fragment>
+                )}
+              </div>
+              <div className={classes["form-section"]}>
+                <p id={classes["already-have-account"]}>
+                  Already have an account? <Link to="/login">Sign in</Link>
+                </p>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </section>
   );
 };
 
